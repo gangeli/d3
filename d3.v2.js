a2b144d8bb0455dc832bc0a43e3e0fa50c553cc1
Gabor Angeli <angeli@cs.stanford.edu>
2012-11-25 15:24:41 -0800

    Compilation and initial example

--- d3.v2.js
+++ d3.v2.js
@@ -6033,6 +6033,46 @@
     };
     return bonne.origin([ 0, 0 ]).parallel(45);
   };
+  d3.geo.composite = function() {
+    function composite(coordinates) {
+      var t = n * (d3_geo_radians * coordinates[0] - lng0), p = Math.sqrt(C - 2 * n * Math.sin(d3_geo_radians * coordinates[1])) / n;
+      return [ scale * p * Math.sin(t) + translate[0], scale * (p * Math.cos(t) - p0) + translate[1] ];
+    }
+    function reload() {
+      var phi1 = d3_geo_radians * parallels[0], phi2 = d3_geo_radians * parallels[1], lat0 = d3_geo_radians * origin[1], s = Math.sin(phi1), c = Math.cos(phi1);
+      lng0 = d3_geo_radians * origin[0];
+      n = .5 * (s + Math.sin(phi2));
+      C = c * c + 2 * n * s;
+      p0 = Math.sqrt(C - 2 * n * Math.sin(lat0)) / n;
+      return composite;
+    }
+    var origin = [ -98, 38 ], parallels = [ 29.5, 45.5 ], scale = 1e3, translate = [ 480, 250 ], lng0, n, C, p0;
+    composite.invert = function(coordinates) {
+      var x = (coordinates[0] - translate[0]) / scale, y = (coordinates[1] - translate[1]) / scale, p0y = p0 + y, t = Math.atan2(x, p0y), p = Math.sqrt(x * x + p0y * p0y);
+      return [ (lng0 + t / n) / d3_geo_radians, Math.asin((C - p * p * n * n) / (2 * n)) / d3_geo_radians ];
+    };
+    composite.origin = function(x) {
+      if (!arguments.length) return origin;
+      origin = [ +x[0], +x[1] ];
+      return reload();
+    };
+    composite.parallels = function(x) {
+      if (!arguments.length) return parallels;
+      parallels = [ +x[0], +x[1] ];
+      return reload();
+    };
+    composite.scale = function(x) {
+      if (!arguments.length) return scale;
+      scale = +x;
+      return composite;
+    };
+    composite.translate = function(x) {
+      if (!arguments.length) return translate;
+      translate = [ +x[0], +x[1] ];
+      return composite;
+    };
+    return reload();
+  };
   d3.geo.equirectangular = function() {
     function equirectangular(coordinates) {
       var x = coordinates[0] / 360, y = -coordinates[1] / 360;
