6dcd939c2ea687d73319e23394df89e95ba53031
Gabor Angeli <angeli@cs.stanford.edu>
2012-11-25 17:25:26 -0800

    Some sort of transitioning works

--- src/geo/composite.js
+++ src/geo/composite.js
@@ -4,74 +4,87 @@
 //
 // Implemented by Sukolsak Sakshuwong and Gabor Angeli
 
-d3.geo.composite = function() {
-  var mode = "orthographic", // or stereographic, gnomonic, equidistant or equalarea
-      origin,
-      scale = 200,
-      translate = [480, 250],
-      x0,
-      y0,
-      cy0,
-      sy0;
+var d3_geo_radians = Math.PI / 180;
 
-  function composite(coordinates) {
-    var x1 = coordinates[0] * d3_geo_radians - x0,
-        y1 = coordinates[1] * d3_geo_radians,
-        cx1 = Math.cos(x1),
-        sx1 = Math.sin(x1),
-        cy1 = Math.cos(y1),
-        sy1 = Math.sin(y1),
-        cc = mode !== "orthographic" ? sy0 * sy1 + cy0 * cy1 * cx1 : null,
-        c,
-        k = mode === "stereographic" ? 1 / (1 + cc)
-          : mode === "gnomonic" ? 1 / cc
-          : mode === "equidistant" ? (c = Math.acos(cc), c ? c / Math.sin(c) : 0)
-          : mode === "equalarea" ? Math.sqrt(2 / (1 + cc))
-          : 1,
-        x = k * cy1 * sx1,
-        y = k * (sy0 * cy1 * cx1 - cy0 * sy1);
-    return [
-      scale * x + translate[0],
-      scale * y + translate[1]
-    ];
+d3.geo.composite = function(viewport) {
+  var origin,
+      scale  = 1.0,
+      width  = viewport[2] - viewport[0],
+      height = viewport[3] - viewport[1],
+      smaller_dimension = Math.min(width, height),
+      viewport_center = [viewport[0] + width/2, viewport[1] + height/2]
+ 
+  var albers   = d3.geo.albers(),
+      mercator = d3.geo.mercator();
+  albers.translate(viewport_center);
+  mercator.translate(viewport_center);
+
+  function composite(coordinates_degrees) {
+    var lon = coordinates_degrees[0] * d3_geo_radians - origin[0],
+        lat = coordinates_degrees[1] * d3_geo_radians - origin[1],
+        clon = Math.cos(lon),
+        slon = Math.sin(lon),
+        clat = Math.cos(lat),
+        slat = Math.sin(lat);
+    // Encompasses Hammer (B = 2) and Lambert Azimuthal (B = 1)
+    function generalized_hammer(B) {
+      var sqrt2 = Math.sqrt(2),
+          sin_lon_over_b = Math.sin(lon / B),
+          cos_lon_over_b = Math.cos(lon / B),
+          nu = 4.0 * Math.sqrt(1 + clat * cos_lon_over_b)
+          x = B * sqrt2 * clat * sin_lon_over_b / nu,
+          y = - sqrt2 * slat / nu;
+      return [
+        scale * smaller_dimension * x  + viewport_center[0],
+        scale * smaller_dimension * y  + viewport_center[1]
+      ];
+    }
+    // Decision Tree
+    if (scale <= 1.5) {
+      return generalized_hammer(2.0);
+    } else if (scale <= 2.0) {
+      return generalized_hammer( 2.0 - (scale-1.5) * 2.0 );
+    } else if (scale <= 4.0) {
+      return generalized_hammer(1.0);
+    } else if (scale <= 13) {
+      return albers(coordinates_degrees);
+    } else if (scale <= 15) {
+      var lambda = (scale - 13.0) / 2.0,
+          a = albers(coordinates_degrees),
+          b = mercator(coordinates_degrees);
+      return [(1.0 - lambda)*a[0] + lambda*b[0],
+              (1.0 - lambda)*a[1] + lambda*b[1]];
+    } else {
+      return mercator(coordinates_degrees);
+    }
   }
 
   composite.invert = function(coordinates) {
-    var x = (coordinates[0] - translate[0]) / scale,
-        y = (coordinates[1] - translate[1]) / scale,
-        p = Math.sqrt(x * x + y * y),
-        c = mode === "stereographic" ? 2 * Math.atan(p)
-          : mode === "gnomonic" ? Math.atan(p)
-          : mode === "equidistant" ? p
-          : mode === "equalarea" ? 2 * Math.asin(.5 * p)
-          : Math.asin(p),
-        sc = Math.sin(c),
-        cc = Math.cos(c);
-    return [
-      (x0 + Math.atan2(x * sc, p * cy0 * cc + y * sy0 * sc)) / d3_geo_radians,
-      Math.asin(cc * sy0 - (p ? (y * sc * cy0) / p : 0)) / d3_geo_radians
-    ];
+    console.log("I was called");
+    // TODO(gangeli): Implement Me!
+    return [0.0, 0.0]
   };
 
-  composite.origin = function(x) {
-    if (!arguments.length) return origin;
-    origin = x;
-    x0 = origin[0] * d3_geo_radians;
-    y0 = origin[1] * d3_geo_radians;
-    cy0 = Math.cos(y0);
-    sy0 = Math.sin(y0);
+  composite.origin = function(origin_degrees) {
+    if (!arguments.length) {
+      return [
+        origin[0] / d3_geo_radians,
+        origin[1] / d3_geo_radians
+      ];
+    }
+    origin = [
+      origin_degrees[0] * d3_geo_radians,
+      origin_degrees[1] * d3_geo_radians
+    ]
+    albers.origin(origin_degrees);
     return composite;
   };
 
   composite.scale = function(x) {
     if (!arguments.length) return scale;
     scale = +x;
-    return composite;
-  };
-
-  composite.translate = function(x) {
-    if (!arguments.length) return translate;
-    translate = [+x[0], +x[1]];
+    albers.scale(x * 100); // TODO(gangeli) this scaling factor is wrong
+    mercator.scale(x * smaller_dimension);
     return composite;
   };
 
