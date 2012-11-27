609ca4e358d524533c04efcf969da9d0570ab7cf
Sukolsak <sukolsak@dn0a210218.sunet>
2012-11-26 20:48:30 -0800

    Implemented hammer.invert.

--- src/geo/hammer.js
+++ src/geo/hammer.js
@@ -19,15 +19,14 @@ d3.geo.hammer = function(B) {
     if (lon < -Math.PI) {
       lon += Math.PI * 2.0;
       have_wrapped = true;
-    }
-    if (lon > Math.PI) {
+    } else if (lon > Math.PI) {
       lon -= Math.PI * 2.0;
       have_wrapped = true;
     }
     var sqrt2 = Math.sqrt(2),
         sin_lon_over_b = Math.sin(lon / B),
         cos_lon_over_b = Math.cos(lon / B),
-        nu = Math.sqrt(1 + clat * cos_lon_over_b)
+        nu = Math.sqrt(1 + clat * cos_lon_over_b),
         x = B * sqrt2 * clat * sin_lon_over_b / nu,
         y = - sqrt2 * slat / nu;
     if( return_wrap ) {
@@ -38,16 +37,45 @@ d3.geo.hammer = function(B) {
       ];
     } else {
       return [
-        scale * 1.5 * x + translate[0],
-        scale * 1.5 * y + translate[1]
+        scale * 0.5 * x + translate[0],
+        scale * 0.5 * y + translate[1]
       ];
     }
   }
+  
+  function aasin(v) {
+    var ONE_TOL = 1.00000000000001;
+    var av = Math.abs(v);
+    if (av >= 1) {
+      if (av > ONE_TOL) {
+        return NaN;
+      }
+      return v < 0 ? -Math.PI / 2 : Math.PI / 2;
+    }
+    return Math.asin(v);
+  }
+
+  function aatan2(n, d) {
+    var ATOL = 1.0e-50;
+    return ((Math.abs(n) < ATOL && Math.abs(d) < ATOL) ? 0 : Math.atan2(n, d));
+  }
 
   hammer.invert = function(coordinates) {
-    console.log("invert hammer");
-    // TODO(gangeli): Implement Me!
-    return [0.0, 0.0]
+    var x = (coordinates[0] - translate[0]) / (scale * 0.5),
+    y = -(coordinates[1] - translate[1]) / (scale * 0.5);
+    var wx = x / B;
+    var EPS10 = 1.e-10;
+    var lon, lat;
+    var z = Math.sqrt(1 - 0.25 * (wx * wx + y * y));
+    var zz2_1 = 2 * z * z - 1;
+    if(Math.abs(zz2_1) < EPS10) {
+      lon = NaN;
+      lat = NaN;
+    } else {
+      lon = aatan2(wx * z, zz2_1) * B;
+      lat = aasin(z * y);
+    }
+    return [(lon + origin[0]) / d3_geo_radians, (lat + origin[1]) / d3_geo_radians];
   };
 
   hammer.origin = function(origin_degrees) {
